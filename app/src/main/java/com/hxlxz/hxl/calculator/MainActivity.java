4ac3b40a14f3d955bc8a0edc79a9c515a3adbd59
Xianglong He <admin@hxlxz.com>
2016-08-27 21:50:45 +0800

    重构

--- app/src/main/java/com/hxlxz/hxl/calculator/MainActivity.java
+++ app/src/main/java/com/hxlxz/hxl/calculator/MainActivity.java
@@ -1,8 +1,10 @@
 package com.hxlxz.hxl.calculator;
 
+import android.icu.text.IDNA;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
@@ -15,157 +17,64 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void OnButton1Press(View view) {
-        EditText editText1 = (EditText) findViewById(R.id.editText1);
-        EditText editText2 = (EditText) findViewById(R.id.editText2);
+    public void OnButtonPress_Num(View view) {
+        TextView textView1 = (TextView) findViewById((R.id.textView1));
+        TextView textView3 = (TextView) findViewById((R.id.textView3));
         if (!FirstNumOK)
-            editText1.setText(editText1.getText().append("1"));
+            textView1.setText(textView1.getText().toString() + ((Button)(findViewById(view.getId()))).getText());
         else
-            editText2.setText(editText2.getText().append("1"));
+            textView3.setText(textView3.getText().toString() + ((Button)(findViewById(view.getId()))).getText());
     }
 
-    public void OnButton2Press(View view) {
-        EditText editText1 = (EditText) findViewById(R.id.editText1);
-        EditText editText2 = (EditText) findViewById(R.id.editText2);
-        if (!FirstNumOK)
-            editText1.setText(editText1.getText().append("2"));
-        else
-            editText2.setText(editText2.getText().append("2"));
-    }
-
-    public void OnButton3Press(View view) {
-        EditText editText1 = (EditText) findViewById(R.id.editText1);
-        EditText editText2 = (EditText) findViewById(R.id.editText2);
-        if (!FirstNumOK)
-            editText1.setText(editText1.getText().append("3"));
-        else
-            editText2.setText(editText2.getText().append("3"));
-    }
-
-    public void OnButton4Press(View view) {
-        EditText editText1 = (EditText) findViewById(R.id.editText1);
-        EditText editText2 = (EditText) findViewById(R.id.editText2);
-        if (!FirstNumOK)
-            editText1.setText(editText1.getText().append("4"));
-        else
-            editText2.setText(editText2.getText().append("4"));
-    }
-
-    public void OnButton5Press(View view) {
-        EditText editText1 = (EditText) findViewById(R.id.editText1);
-        EditText editText2 = (EditText) findViewById(R.id.editText2);
-        if (!FirstNumOK)
-            editText1.setText(editText1.getText().append("5"));
-        else
-            editText2.setText(editText2.getText().append("5"));
-    }
-
-    public void OnButton6Press(View view) {
-        EditText editText1 = (EditText) findViewById(R.id.editText1);
-        EditText editText2 = (EditText) findViewById(R.id.editText2);
-        if (!FirstNumOK)
-            editText1.setText(editText1.getText().append("6"));
-        else
-            editText2.setText(editText2.getText().append("6"));
-    }
-
-    public void OnButton7Press(View view) {
-        EditText editText1 = (EditText) findViewById(R.id.editText1);
-        EditText editText2 = (EditText) findViewById(R.id.editText2);
-        if (!FirstNumOK)
-            editText1.setText(editText1.getText().append("7"));
-        else
-            editText2.setText(editText2.getText().append("7"));
-    }
-
-    public void OnButton8Press(View view) {
-        EditText editText1 = (EditText) findViewById(R.id.editText1);
-        EditText editText2 = (EditText) findViewById(R.id.editText2);
-        if (!FirstNumOK)
-            editText1.setText(editText1.getText().append("8"));
-        else
-            editText2.setText(editText2.getText().append("8"));
-    }
-
-    public void OnButton9Press(View view) {
-        EditText editText1 = (EditText) findViewById(R.id.editText1);
-        EditText editText2 = (EditText) findViewById(R.id.editText2);
-        if (!FirstNumOK)
-            editText1.setText(editText1.getText().append("9"));
-        else
-            editText2.setText(editText2.getText().append("9"));
-    }
-
-    public void OnButton0Press(View view) {
-        EditText editText1 = (EditText) findViewById(R.id.editText1);
-        EditText editText2 = (EditText) findViewById(R.id.editText2);
-        if (!FirstNumOK)
-            editText1.setText(editText1.getText().append("0"));
-        else
-            editText2.setText(editText2.getText().append("0"));
-    }
-
-    public void OnButtonPlusPress(View view) {
-        TextView textView = (TextView) findViewById((R.id.textView));
-        textView.setText("+");
-        FirstNumOK = true;
-    }
-
-    public void OnButtonMinusPress(View view) {
-        TextView textView = (TextView) findViewById((R.id.textView));
-        textView.setText("-");
-        FirstNumOK = true;
-    }
-
-    public void OnButtonChenPress(View view) {
-        TextView textView = (TextView) findViewById((R.id.textView));
-        textView.setText("*");
-        FirstNumOK = true;
-    }
-
-    public void OnButtonDividePress(View view) {
-        TextView textView = (TextView) findViewById((R.id.textView));
-        textView.setText("/");
+    public void OnButtonPress_Operator(View view) {
+        TextView textView2 = (TextView) findViewById((R.id.textView2));
+        textView2.setText(((Button)(findViewById(view.getId()))).getText());
         FirstNumOK = true;
     }
 
     public void OnButtonEnterPress(View view) {
-        EditText editText1 = (EditText) findViewById(R.id.editText1);
-        EditText editText2 = (EditText) findViewById(R.id.editText2);
-        TextView textView = (TextView) findViewById((R.id.textView));
+        TextView textView1 = (TextView) findViewById((R.id.textView1));
         TextView textView2 = (TextView) findViewById((R.id.textView2));
-        double a = Double.parseDouble(editText1.getText().toString());
-        double b = Double.parseDouble(editText2.getText().toString());
-        double c = 0;
-        switch (textView.getText().toString().toCharArray()[0]) {
-            case '+':
-                c = a + b;
-                break;
-            case '-':
-                c = a - b;
-                break;
-            case '*':
-                c = a * b;
-                break;
-            case '/':
-                c = a / b;
-                break;
-            default:
-                break;
+        TextView textView3 = (TextView) findViewById((R.id.textView3));
+        TextView textView4 = (TextView) findViewById((R.id.textView4));
+        try {
+            double a = Double.parseDouble(textView1.getText().toString());
+            double b = Double.parseDouble(textView3.getText().toString());
+            double c = 0;
+            switch (textView2.getText().toString().toCharArray()[0]) {
+                case '+':
+                    c = a + b;
+                    break;
+                case '-':
+                    c = a - b;
+                    break;
+                case '*':
+                    c = a * b;
+                    break;
+                case '/':
+                    c = a / b;
+                    break;
+                default:
+                    break;
+            }
+            textView4.setText("=" + String.valueOf(c));
         }
-        textView2.setText("=" + String.valueOf(c));
+        catch(Exception e) {
+
+        }
+
         FirstNumOK = false;
     }
 
     public void OnButtonCPress(View view) {
-        EditText editText1 = (EditText) findViewById(R.id.editText1);
-        EditText editText2 = (EditText) findViewById(R.id.editText2);
-        TextView textView = (TextView) findViewById((R.id.textView));
+        TextView textView1 = (TextView) findViewById((R.id.textView1));
         TextView textView2 = (TextView) findViewById((R.id.textView2));
-        editText1.setText("");
-        editText2.setText("");
-        textView.setText("");
+        TextView textView3 = (TextView) findViewById((R.id.textView3));
+        TextView textView4 = (TextView) findViewById((R.id.textView4));
+        textView1.setText("");
         textView2.setText("");
+        textView3.setText("");
+        textView4.setText("");
         FirstNumOK = false;
     }
 }
